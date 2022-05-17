d1ca88e37f93f0d81e2ef4be6f0d8ab92b4fbf49
Schifferking <schifferking@hotmail.com>
2022-05-17 11:46:07 -0500

    Add logic to substrings function
    It has two parameters, one is a word or a string with many words and
    the second is a "dictionary" or an array of substrings.
    
    The objective is to create a hash that contains all the dictionary
    substrings found in the word(s) as keys and the count of each one
    as values.
    
    To do this, first it creates an array of the word(s), then convert them
    to lowercase because it is irrelevant to match the case in this project.
    
    Then create a new array that will store the arrays of the substrings
    found in the words. For that it will iterate each word first and then
    for each iteration there is another one to iterate each element of the
    dictionary to see if an element is substring of the current word; if it
    is then it pushes it to the current array and when it finishes it pushes
    that array to the other one.
    
    When that process has finished, the main arary is now an array of
    arrays; to create the hash first it is needed to flatten it. Now done
    that, there is another iteration with the reduce method where it creates
    a new entry in the hash when a new substring is found, in case it
    already exists, just adds one to the value of that element. And when
    this loop ends it returns the hash.

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,4 +1,22 @@
-def substrings(word, substrings_list)
-    word
-    substrings_list
+def substrings(word, dictionary)
+  #Split phrase in array and convert to lowercase
+  words_list = word.split
+  words_list.map! { |word| word.downcase }
+
+  matches = []
+  words_list.each do |word|
+    # Get all substrings found based on the current word and store in an array
+    matches.push(dictionary.select do |substring|
+      substring if (word.include?(substring))
+    end)
+  end
+
+  matches = matches.flatten
+
+  # Create the hash of substrings as keys and count as values
+  matches.reduce(Hash.new(0)) do |substrings_hash, substring|
+    substrings_hash[substring] += 1
+    substrings_hash
+  end
+
 end
